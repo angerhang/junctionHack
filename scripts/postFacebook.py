636099198c50897c1e2344a803a08f5260c3677c
angerhang <514140809@qq.com>
2017-11-26 10:59:25 +0200

    update token

--- scripts/postFacebook.py
+++ scripts/postFacebook.py
@@ -5,8 +5,8 @@ def main():
   # Fill in the values noted in previous steps here
   cfg = {
     "page_id"      : "1956961034578187",  # Step 1
-    "access_token" : "EAASXm01iyRMBACeZCc3TnDki3Dr3yLozJQcDBpuX4oAUxq4J0OM3KsKKjr8vyHPJuuVTVyLCOQzWRmvCvpOnh79aPmed" +\
-                     "wDOphvkKyvKtcviOvLEiOb6C8j5G51zD8aZBee5v67YUHz4cb4rX1uK4L5XOwMCbOuWpZCq1spVKlbZApslKIpJebUuNMNu8JucZD"
+    "access_token" : "EAACEdEose0cBAFu2MjzoQkQLTg76ZCgzdLyoYd4PRTkC8MYAjEl7YPdT" + \
+    "7Thl17snnZAGPniKWgi1ADydFlHZACT5ZCMgOlXoCeb1iksr69jclhUqTFWxTv1Noay0Rh71VxYXNAwcvIRfZCz1a13bCMCWSylQV7jBpZBnWpbPbGfGVF5QFlg8AMOrsPaI27xZC8ZD"
   }
 
   api = get_api(cfg)
@@ -20,8 +20,8 @@ def get_api(cfg):
   graph = facebook.GraphAPI(cfg['access_token'])
   # Get page token to post as the page. You can skip
   # the following if you want to post as yourself.
-  page_access_token = "EAASXm01iyRMBAHaHVq4Eq9Xp3vFWFA0ZB2ZBCMfdW0K41bOOTBUhUCG9FFztoOBux5bOhiwSqtPZCvLjtEMhmBUaCUwb6CDDVG" + \
-  "ahZAtFkdgKb71u3vsXXwnLkzVyZCsSj1b3RAhPf0gq6lbKGYm4SM8MQSannvQRwLfYL4UIKSmp0mROMwpxZCMAZCMcxRJkmRl7EHyRHLKKAZDZD"
+  page_access_token = "EAASXm01iyRMBAHzaLjEZA57BkoTRZA0Rif1XBcG1GKodLP0op1PeODzJqyyZCNM0we6N5tdzkgyYZAMWBUParVE0TnJtqbh3bznuzZA8D" + \
+  "5tsFLRTtaeuurZCltWCNyQZCw0mpYRbGVKil5cwqemPm3F1bNXfYRsI9jko0ukgAJHQVLaiQxptXrNyxqjz73KxjEv04PF1L6LpgZDZD"
   graph = facebook.GraphAPI(page_access_token)
   return graph
   # You can also skip the above if you get a page token:
